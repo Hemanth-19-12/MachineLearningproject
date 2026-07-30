872b768a0c9ad6c6afa6f0e40b267bf0dc53f168
Hemanth-19-12 <hemanth.grvd@gmail.com>
2026-07-30 18:45:56 +0530

    EDA and Problem statement

--- src/logger.py
+++ src/logger.py
@@ -2,20 +2,13 @@ import logging
 import os
 from datetime import datetime
 
-# Define the log filename
-LOG_FILE = f"{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.log"
-
-# FIX: Create the path to the 'logs' folder only
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
-
-# Define the final path to the file inside that folder
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -  %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
-
